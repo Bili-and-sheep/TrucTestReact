2bb1255b6114fe81ce47e3c5cef5a4951dc23320
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2025-01-20 14:54:59 +0100

    Expo Tutorial Build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,26 @@
 import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+import Button from '@/components/Button';
+
 import { View, Text, StyleSheet } from 'react-native';
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      {/* <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>
         Go to About screen
-      </Link>
+      </Link> */}
     </View>
   );
 }
@@ -26,6 +39,19 @@ const styles = StyleSheet.create({
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
+    alignItems: 'center',
     color: '#fff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
