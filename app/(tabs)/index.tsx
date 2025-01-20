d7b3e9b6558018842f386a7d415583ac3b8f1c55
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2025-01-20 15:29:20 +0100

    Expo Tutorial Use an image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'expo-router';
 import { Image } from 'expo-image';
+import { useState } from 'react';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+
 
 import { View, Text, StyleSheet } from 'react-native';
 import ImageViewer from "@/components/ImageViewer";
@@ -8,13 +11,29 @@ import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function HomeScreen() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      // console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       {/* <Text style={styles.text}>Home screen</Text>
