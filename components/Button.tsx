13518d2396ed496fd02e52faad276d01977f0bdd
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2025-01-20 16:42:46 +0100

    Expo Tutorial Create a modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -29,7 +29,9 @@ export default function Button({ label, theme, onPress }: Props) {
 
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.' + { label })}>
+            {/* <Pressable style={styles.button} onPress={() => alert('You pressed a button.' + { label })}>
+             */}
+            <Pressable style={styles.button} onPress={onPress} >
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
