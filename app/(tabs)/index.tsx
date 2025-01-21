79f8c17f69ff78a19f60af958a2afa99104fdf13
Bili-and-sheep <70603677+Bili-and-sheep@users.noreply.github.com>
2025-01-21 14:54:41 +0100

    Expo tutorial Handle platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,9 @@ import { Image } from 'expo-image';
 import { type ImageSource } from 'expo-image';
 import { useState, useRef } from 'react';
 import { captureRef } from 'react-native-view-shot';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import domtoimage from 'dom-to-image';
 
 
 import * as ImagePicker from 'expo-image-picker';
@@ -62,18 +63,35 @@ export default function HomeScreen() {
     setIsModalVisible(false);
   };
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Saved!');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
